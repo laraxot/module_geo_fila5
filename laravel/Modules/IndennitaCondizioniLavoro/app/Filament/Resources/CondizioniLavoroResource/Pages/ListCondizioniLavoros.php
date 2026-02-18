3b4f7377333591cc5e6a02450423f53a8cb70ea5
marco xot <marco.sottana@gmail.com>
2026-02-18 12:09:32 +0100

    refactor: improve type safety in ListCondizioniLavoros
    - Added Symfony\Component\HttpFoundation\Response import for better type annotations.
    - Updated action method in getHeaderActions to specify return type as Response.

--- laravel/Modules/IndennitaCondizioniLavoro/app/Filament/Resources/CondizioniLavoroResource/Pages/ListCondizioniLavoros.php
+++ laravel/Modules/IndennitaCondizioniLavoro/app/Filament/Resources/CondizioniLavoroResource/Pages/ListCondizioniLavoros.php
@@ -24,6 +24,7 @@ use Modules\Ptv\Actions\GetValutatoriOptionsByWhere;
 use Modules\Ptv\Filament\Tables\Columns\WorkerColumn;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 use Override;
+use Symfony\Component\HttpFoundation\Response;
 
 class ListCondizioniLavoros extends XotBaseListRecords
 {
@@ -95,7 +96,7 @@ class ListCondizioniLavoros extends XotBaseListRecords
             'exportPdf' => Action::make('exportPdf')
                 ->label('Pdf ')
                 ->icon('heroicon-s-document')
-                ->action(function () {
+                ->action(function (): Response {
                     $tableFilters = is_array($this->tableFilters) ? $this->tableFilters : [];
 
                     return app(MakePdf::class)->execute($tableFilters);
