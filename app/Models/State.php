5434d2c4571d50cbf3499673bb79ff64d0c7e56b
marco76tv <marco.sottana@gmail.com>
2026-03-10 16:55:38 +0100

    .

--- app/Models/State.php
+++ app/Models/State.php
@@ -11,15 +11,21 @@ use Modules\Xot\Contracts\ProfileContract;
 /**
  * @property ProfileContract|null $creator
  * @property ProfileContract|null $updater
- *
  * @method static Builder<static>|State newModelQuery()
  * @method static Builder<static>|State newQuery()
  * @method static Builder<static>|State query()
- *
  * @property ProfileContract|null $deleter
- *
  * @method static StateFactory factory($count = null, $state = [])
- *
+ * @property string $id
+ * @property string $state
+ * @property string $state_code
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static Builder<static>|State whereCreatedAt($value)
+ * @method static Builder<static>|State whereId($value)
+ * @method static Builder<static>|State whereState($value)
+ * @method static Builder<static>|State whereStateCode($value)
+ * @method static Builder<static>|State whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class State extends BaseModel
