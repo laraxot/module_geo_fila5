b55f320c3d9f7678b15eea40712023f4cdbac273
Malebestia <malebestia@yahoo.it>
2026-03-26 09:12:44 +0100

    .

--- tests/Unit/Actions/GoogleMaps/GetGeocodingDataActionTest.php
+++ tests/Unit/Actions/GoogleMaps/GetGeocodingDataActionTest.php
@@ -7,10 +7,13 @@ namespace Modules\Geo\Tests\Unit\Actions\GoogleMaps;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
 use Modules\Geo\Tests\LightTestCase;
 
 uses(LightTestCase::class);
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
@@ -19,10 +22,14 @@ use GuzzleHttp\Psr7\Response;
 use Modules\Geo\Actions\GoogleMaps\GetGeocodingDataAction;
 use Modules\Geo\Datas\GeocodingData;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 use Modules\Geo\Tests\LightTestCase;
 
 uses(LightTestCase::class);
 =======
+=======
+use Modules\Geo\Tests\LightTestCase;
+>>>>>>> Stashed changes
 
 >>>>>>> Stashed changes
 
