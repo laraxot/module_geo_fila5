df60ba176de52bdbd92687f21642dabf1f83901b
marco76tv <marco.sottana@gmail.com>
2026-04-21 08:57:30 +0200

    .

--- tests/Unit/Filament/FilamentComponentsTest.php
+++ tests/Unit/Filament/FilamentComponentsTest.php
@@ -8,11 +8,21 @@ uses(\Modules\Geo\Tests\TestCase::class);
 
 use Modules\Geo\Filament\Actions\UpdateCoordinatesBulkAction;
 use Modules\Geo\Filament\Forms\Components\AddressField;
+use Modules\Geo\Filament\Forms\Components\MapPicker;
+use Modules\Geo\Filament\Widgets\GeoMapWidget;
 use Modules\Geo\Filament\Widgets\LatLngWidget;
 use Modules\Geo\Filament\Widgets\LocationWidget;
 
 test('AddressField can be instantiated', function () {
     $field = AddressField::make('address');
+
+    expect($field)->toBeObject();
+});
+
+test('MapPicker can be instantiated', function () {
+    $field = MapPicker::make('map_picker')
+        ->latitude('latitude')
+        ->longitude('longitude');
 
     expect($field)->toBeObject();
 });
@@ -23,6 +33,10 @@ test('LocationWidget can be instantiated', function () {
 
 test('LatLngWidget can be instantiated', function () {
     expect(class_exists(LatLngWidget::class))->toBeTrue();
+});
+
+test('GeoMapWidget can be instantiated', function () {
+    expect(class_exists(GeoMapWidget::class))->toBeTrue();
 });
 
 test('UpdateCoordinatesBulkAction can be instantiated', function () {
