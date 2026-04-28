816e897bd051f0f2c9f79307038e166f0b935f16
marco76tv <marco76tv@users.noreply.github.com>
2026-04-28 19:45:56 +0000

    Check & fix styling

--- tests/Unit/Actions/GoogleMaps/CalculateTravelTimeActionTest.php
+++ tests/Unit/Actions/GoogleMaps/CalculateTravelTimeActionTest.php
@@ -16,7 +16,7 @@ use Modules\Geo\Datas\LocationData;
 use Modules\Geo\Datas\TravelTimeData;
 
 beforeEach(function () {
-    $this->mockHandler = new MockHandler;
+    $this->mockHandler = new MockHandler();
     $handlerStack = HandlerStack::create($this->mockHandler);
     $client = new Client(['handler' => $handlerStack]);
     $this->action = new CalculateTravelTimeAction($client);
