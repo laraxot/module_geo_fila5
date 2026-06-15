5d15e059d71d50fb3074a828c0fbd2c67d48f869
marco xot <marco.sottana@gmail.com>
2026-06-15 22:18:05 +0200

    Add PHPStan stubs for Media, Notify, Tenant, and User modules
    Created new PHPStan stubs for the following modules to enhance IDE support:
    - Media: IdeHelperMedia, IdeHelperMediaConvert, IdeHelperTemporaryUpload
    - Notify: IdeHelperContact, IdeHelperMailTemplate, IdeHelperNotification, and others
    - Tenant: IdeHelperDomain
    - User: IdeHelperAuthentication, IdeHelperDevice, IdeHelperRoleHasPermission, and others
    
    These stubs facilitate better type analysis and autocompletion in IDEs, improving developer experience and code quality.

---
+++ laravel/phpstan-stubs/TenantIdeHelperStubs.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Tenant\Models;
+
+/**
+ * PHPStan stubs for barryvdh/laravel-ide-helper generated classes.
+ *
+ * @internal analysis-only
+ */
+class IdeHelperDomain {}
---
+++ laravel/phpstan-stubs/UserIdeHelperStubs.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\User\Models;
+
+/**
+ * PHPStan stubs for barryvdh/laravel-ide-helper generated classes.
+ *
+ * @internal analysis-only
+ */
+class IdeHelperAuthentication {}
+class IdeHelperAuthenticationLog {}
+class IdeHelperDevice {}
+class IdeHelperDeviceProfile {}
+class IdeHelperDeviceUser {}
+class IdeHelperExtra {}
+class IdeHelperFeature {}
+class IdeHelperMembership {}
+class IdeHelperModelHasPermission {}
+class IdeHelperModelHasRole {}
+class IdeHelperNotification {}
+class IdeHelperOauthAccessToken {}
+class IdeHelperPasswordReset {}
+class IdeHelperPermissionRole {}
+class IdeHelperPermissionUser {}
+class IdeHelperProfile {}
+class IdeHelperProfileTeam {}
+class IdeHelperRoleHasPermission {}
+class IdeHelperSocialProvider {}
+class IdeHelperSocialiteUser {}
+class IdeHelperSsoProvider {}
+class IdeHelperTeam {}
+class IdeHelperTeamInvitation {}
+class IdeHelperTeamPermission {}
+class IdeHelperTeamUser {}
+class IdeHelperTenant {}
+class IdeHelperUser {}
