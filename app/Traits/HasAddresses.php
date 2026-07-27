ec3e94cf9ca0844983e0f83322c47077baee94b3
marco76tv <marco76tv@users.noreply.github.com>
2026-07-27 10:29:19 +0000

    Check & fix styling

--- app/Traits/HasAddresses.php
+++ app/Traits/HasAddresses.php
@@ -75,7 +75,7 @@ trait HasAddresses
     /**
      * Aggiunge un nuovo indirizzo.
      *
-     * @param  array<string, mixed>  $data
+     * @param array<string, mixed> $data
      */
     public function addAddress(array $data, bool $isPrimary = false): Address
     {
