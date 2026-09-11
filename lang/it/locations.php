6ac358a48c5e1183a737a3a8e6c7604778249b9b
Marco Xot <marco76tv@hotmail.com>
2026-09-11 19:07:19 +0200

    .

--- lang/it/locations.php
+++ lang/it/locations.php
@@ -7,5 +7,11 @@ return [
         'id' => ['label' => 'id'],
         'name' => ['label' => 'name'],
         'created_at' => ['label' => 'created_at'],
+        'street' => ['label' => 'street'],
+        'city' => ['label' => 'city'],
+        'zip' => ['label' => 'zip'],
+        'processed' => ['label' => 'processed'],
+        'lat' => ['label' => 'lat'],
+        'lng' => ['label' => 'lng'],
     ],
 ];
