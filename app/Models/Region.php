0b38631c9743a3decba27a2b6f8df752de433a6e
Marco Xot <marco76tv@hotmail.com>
2026-09-14 18:40:30 +0200

    docs: rimosso PHPDoc @use HasXotFactory e import Factory da PlaceType, Province, Region
    - Rimosso `@use HasXotFactory<Factory<static>>` PHPDoc da PlaceType, Province, Region
    - Rimosso import `Illuminate\Database\Eloquent\Factories\Factory` non necessario
    - Semplificato docs/index.md rimuovendo contenuto duplicato vecchio indice (da 829 righe a 203)

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -7,7 +7,6 @@ namespace Modules\Geo\Models;
 use Filament\Schemas\Components\Utilities\Get;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\File;
 use Modules\Xot\Actions\Cast\SafeStringCastAction;
@@ -34,7 +33,6 @@ use Sushi\Sushi;
  */
 class Region extends BaseModel
 {
-    /** @use HasXotFactory<Factory<static>> */
     use HasXotFactory;
     use Sushi;
 
