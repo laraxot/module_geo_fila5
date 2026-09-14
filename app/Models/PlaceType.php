216061a3a407850ce2694d42056c782dd764c067
Marco Xot <marco76tv@hotmail.com>
2026-09-14 18:40:30 +0200

    docs: rimosso PHPDoc @use HasXotFactory e import Factory da PlaceType, Province, Region
    - Rimosso `@use HasXotFactory<Factory<static>>` PHPDoc da PlaceType, Province, Region
    - Rimosso import `Illuminate\Database\Eloquent\Factories\Factory` non necessario
    - Semplificato docs/index.md rimuovendo contenuto duplicato vecchio indice (da 829 righe a 203)

--- app/Models/PlaceType.php
+++ app/Models/PlaceType.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Modules\Geo\Models;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 use Modules\Xot\Contracts\ProfileContract;
 use Modules\Xot\Models\Traits\HasXotFactory;
@@ -35,7 +34,6 @@ use Modules\Xot\Models\Traits\HasXotFactory;
  */
 class PlaceType extends BaseModel
 {
-    /** @use HasXotFactory<Factory<static>> */
     use HasXotFactory;
 
     protected $fillable = [
--- app/Models/Province.php
+++ app/Models/Province.php
@@ -7,7 +7,6 @@ namespace Modules\Geo\Models;
 use Filament\Schemas\Components\Utilities\Get;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\File;
@@ -38,7 +37,6 @@ use Sushi\Sushi;
  */
 class Province extends BaseModel
 {
-    /** @use HasXotFactory<Factory<static>> */
     use HasXotFactory;
     use Sushi;
 
