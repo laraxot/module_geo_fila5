ee371ed26c2b55b252acfed74c454afbfaa61f5b
Marco Xot <marco.sottana@gmail.com>
2026-09-24 10:33:57 +0000

    refactor: change static methods to instance methods for form and infolist schemas in Address and Location resources
    Updated the following methods to be instance methods:
    - AddressForm::getFormSchema()
    - AddressInfolist::getInfolistSchema()
    - LocationForm::getFormSchema()
    - LocationInfolist::getInfolistSchema()
    
    This change enhances the flexibility of the resource classes by allowing instance-specific behavior.

--- app/Filament/Resources/LocationResource/Schemas/LocationForm.php
+++ app/Filament/Resources/LocationResource/Schemas/LocationForm.php
@@ -14,7 +14,7 @@ class LocationForm extends XotBaseResourceForm
     /**
      * @return array<int|string, Component>
      */
-    public static function getFormSchema(): array
+    public function getFormSchema(): array
     {
         return [
             'main' => Section::make()
